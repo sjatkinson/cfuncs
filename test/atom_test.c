aed747aebe5773b1809e2218b8f45aa32cfae0ae
sjatkinson <atkinson.steven@gmail.com>
2010-01-04 10:54:35 -0800

    added test to break atom

--- test/atom_test.c
+++ test/atom_test.c
@@ -7,8 +7,14 @@
 #include "cgreen/cgreen.h"
 
 void atom_test() {
-   atom_t a[1028];
+   atom_t a[17];
+   int i;
    assert_true(strcmp(atom_str(&a, "test"), "test") == 0);
    assert_true(atom_str(&a, "test") != atom_str(&a, "test2"));
    assert_equal(atom_str(&a, "foo"), atom_str(&a, "foo"));
+   for(i = 0; i<120; i++) {
+      char t[10];
+      sprintf(t, "test%d", i);
+      assert_equal(atom_str(&a, t), atom_str(&a, t));
+   }
 }
